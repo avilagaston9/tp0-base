11080b53c01c80d15c790de7630160d86d3a2969
avilagaston9 <gaston.avila@lambdaclass.com>
2025-08-23 20:38:32 -0300

    Fix script

--- generar-compose.sh
+++ generar-compose.sh
@@ -41,11 +41,11 @@ do
     entrypoint: /client
     environment:
       - CLI_ID=$i
-      -NOMBRE=\${NOMBRE:-}
-      -APELLIDO=\${APELLIDO:-}
-      -DOCUMENTO=\${DNI:-}
-      -NACIMIENTO=\${NACIMIENTO:-}
-      -NUMERO=\${NUMERO:-}
+      - NOMBRE=\${NOMBRE:-}
+      - APELLIDO=\${APELLIDO:-}
+      - DOCUMENTO=\${DNI:-}
+      - NACIMIENTO=\${NACIMIENTO:-}
+      - NUMERO=\${NUMERO:-}
     networks:
       - testing_net
     depends_on:
