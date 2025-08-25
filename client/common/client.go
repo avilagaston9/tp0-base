a65a86a03b89fc2c5c0344ffe346467e24f95010
avilagaston9 <gaston.avila@lambdaclass.com>
2025-08-25 00:44:54 -0300

    Fix client log

--- client/common/client.go
+++ client/common/client.go
@@ -176,7 +176,7 @@ func (c *Client) StartClientLoop() {
 				)
 			}
 		}
-		log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+		log.Infof("action: exit | result: success | client_id: %v", c.config.ID)
 	}
 }
 
